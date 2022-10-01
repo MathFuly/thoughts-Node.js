9b5ab1a1f15d035aa26aa2288070062176959e87
MatFuly <matheus.fuly.ferreira1@gmail.com>
2022-10-01 18:30:52 -0300

    Mais Configurações

--- index.mjs
+++ index.mjs
@@ -1,4 +1,5 @@
-import express from "express";
+import express, { application } from "express";
+import path from "path";
 import { engine } from "express-handlebars";
 import session from "express-session";
 import FileStore from "session-file-store";
@@ -8,7 +9,53 @@ import conn from "./db/conn.mjs";
 
 const app = express();
 
+// template engine
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
 
+// public path
+app.use(express.static("public"));
+
+// receber resposta do body
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use(express.json());
+
+// session middleware
+app.use(
+  session({
+    name: "session",
+    secret: "nosso_secret",
+    resave: false,
+    saveUninitialized: false,
+    store: new FileStore({
+      logFn: function () {},
+      path: path.join(require("os").tmpdir(), "sessions"),
+    }),
+    cookie: {
+      secure: false,
+      maxAge: 360000,
+      expires: new Date(Date.now() + 360000),
+      httpOnly: true,
+    },
+  })
+);
+
+// flash messages
+app.use(flash());
+
+// set session to res
+app.use((req, res, next) => {
+  if (req.session.userid) {
+    res.locals.session = req.session;
+  }
+
+  next();
+});
 
 conn
   .sync()
