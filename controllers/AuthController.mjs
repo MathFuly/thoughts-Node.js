d9492d4a2638f9722cff1ccb69376565beef41fa
MatFuly <matheus.fuly.ferreira1@gmail.com>
2022-10-03 23:07:03 -0300

    Conclusão da autenticação

--- controllers/AuthController.mjs
+++ controllers/AuthController.mjs
@@ -7,6 +7,39 @@ export default class AuthController {
     res.render("auth/login");
   }
 
+  static async loginPost(req, res) {
+    const { email, password } = req.body;
+
+    //find user
+    const user = await User.findOne({ where: { email: email } });
+
+    if (!user) {
+      req.flash("message", "Usuário não encontrado!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    //check if password match
+    const passwordMatch = bcrypt.compareSync(password, user.password);
+
+    if (!passwordMatch) {
+      req.flash("message", "Senha inválida!");
+      res.render("auth/login");
+
+      return;
+    }
+
+    // initialize session
+    req.session.userid = user.id;
+
+    req.flash("message", "Login efetuado com sucesso!");
+
+    req.session.save(() => {
+      res.redirect("/");
+    });
+  }
+
   static register(req, res) {
     res.render("auth/register");
   }
@@ -60,7 +93,7 @@ export default class AuthController {
   }
 
   static logout(req, res) {
-    req.session.destroy()
-    res.redirect('/login')
+    req.session.destroy();
+    res.redirect("/login");
   }
 }
--- routes/authRoutes.mjs
+++ routes/authRoutes.mjs
@@ -4,6 +4,7 @@ import AuthController from "../controllers/AuthController.mjs";
 const authRouter = express.Router();
 
 authRouter.get("/login", AuthController.login);
+authRouter.post("/login", AuthController.loginPost);
 authRouter.get("/register", AuthController.register);
 authRouter.post("/register", AuthController.registerPost);
 authRouter.get("/logout", AuthController.logout);
